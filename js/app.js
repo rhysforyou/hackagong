5ba3c3feb852bf393660c4ee80beb48f12e8c70a
Rhys Powell <rhys@rpowell.me>
2012-11-24 11:27:46 +1100

    Mocked out the form

--- js/app.js
+++ js/app.js
@@ -1,16 +1,38 @@
-var App = Ember.Application.create();
+var App = Ember.Application.create()
 
-App.ApplicationController = Ember.Controller.extend();
+App.genders = ['guy', 'girl']
+
+App.User = Ember.Object.extend({
+  age: 18,
+  sex: 'guy',
+  ageTimesTwo: function() {
+    return this.age * 2
+  }.property('age')
+})
+
+App.ApplicationController = Ember.Controller.extend()
 App.ApplicationView = Ember.View.extend({
   templateName: 'application'
-});
+})
+
+App.FormController = Ember.Controller.extend()
+App.FormView = Ember.View.extend({
+  templateName: 'form'
+})
 
 App.Router = Ember.Router.extend({
   root: Ember.Route.extend({
     index: Ember.Route.extend({
-      route: '/'
+      route: '/',
+
+      connectOutlets: function(router) {
+        var controller = router.get('applicationController')
+        var context = App.User.create()
+
+        controller.connectOutlet('form', context)
+      }
     })
   })
 })
 
-App.initialize();
+App.initialize()
